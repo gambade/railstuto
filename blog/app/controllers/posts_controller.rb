d1d3ab413d49b205589d53dfaf9d408ecd1a87b9
Sylvan Ravinet <sylvan.ravinet@gambade.com>
2013-08-28 14:34:51 +0200

    fixed

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,24 +1,22 @@
 class PostsController < ApplicationController
 
-def new
-end
+  def new
+  end
 
-def create
-	@post = Post.new(params[:post].permit(:title, :text))
-	
-	@post.save
-	redirect_to @post
-end
-
-private
-	def post_params
-		params.require(:post).permit(:title,:text)
+  def create
+		@post = Post.new(params[:post].permit(:title, :text))
+		
+		@post.save
+		redirect_to @post
 	end
 
-#def show
-	
-	#@post=Post.find(params[:id])
-	#puts 'REGARDE ICI' , @post.inspect
-#end
+	def show
+		@post=Post.find(params[:id])	
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title,:text)
+		end
 
 end
